b418c04de98583d5b4c023cab6ba3e5870b2ccac
barbaraaliverti@gmail.com <barbaraaliverti@gmail.com>
2021-06-22 18:47:09 -0300

    Setup and Class 1

---
+++ src/server.ts
@@ -0,0 +1,15 @@
+import express from "express";
+
+const app = express();
+
+//criar rota
+app.get("/test", (request, response) => {
+    //request: entrada
+    //response: saída
+    return response.send("olá, nlw");
+});
+
+app.post("/test-post", (request, response) => {
+    return response.send("Olá post ")
+})
+app.listen(3000, () => {console.log("Server is running uhul")});
